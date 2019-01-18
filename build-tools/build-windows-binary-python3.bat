ecae197c80e6f31c3fa8d88f78592a5e9a4973ff
diewland <diewland@gmail.com>
2019-01-18 12:01:49 +0700

    add build script for python3

---
+++ build-tools/build-windows-binary-python3.bat
@@ -0,0 +1,3 @@
+rm -r build dist lableImg.spec
+pyinstaller.exe --hidden-import=xml --hidden-import=xml.etree --hidden-import=xml.etree.ElementTree --hidden-import=lxml.etree -D -F -n labelImg -c "..\labelImg.py" -p ..\libs -p ..\
+cp -rf ..\data\ dist\data
